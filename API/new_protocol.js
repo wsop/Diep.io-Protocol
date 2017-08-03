ea5eebc5c4bcfa20f4927f250adc98cf23ddc11c
Faber <miguelkiwi112@gmail.com>
2017-08-03 16:22:03 +0200

    Update new_protocol.js

--- API/new_protocol.js
+++ API/new_protocol.js
@@ -1,10 +1,26 @@
 window.Diep = {
+	debug: {
+    	keys: false
+    },
     nick: ""
 };
 
 function MessageFromClient(buf, offset) {
-    var opcode = buf.readUInt8(0);
+    var opcode = buf.readUInt8(offset);
     switch (opcode) {
+    	case 1 && Diep.debug.keys:
+    		offset += 1;
+    		var flag = buf.readUInt8(offset);
+    		if (flag === 128) console.log("Stopped");
+    		if (flag === 130) console.log("W");
+    		if (flag === 132) console.log("A");
+    		if (flag === 134) console.log("W & A");
+    		if (flag === 136) console.log("S");
+    		if (flag === 140) console.log("A & S");
+    		if (flag === 144) console.log("D");
+    		if (flag === 146) console.log("D & W");
+    		if (flag === 152) console.log("S & D");
+    		break;
         case 2:
             var nick = "";
             for (var i = 0; i < buf.byteLength; i++) {
